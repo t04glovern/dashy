d605cb914aa5a13c3058893081efc13e8f6d731f
Nathan Glover <nathan@glovers.id.au>
2017-06-18 02:04:00 +0800

    Removed the video feed until I can fix the mass download problems

--- api/resources/pubg.py
+++ api/resources/pubg.py
@@ -11,6 +11,7 @@ class PUBG(Resource):
         self.api_key = api_key
         self.platform = platform
         self.pubg_url = "https://pubgtracker.com/api/profile/{}/".format(self.platform)
+        self.steam_url = "https://pubgtracker.com/api/search?steamId="
         self.headers = {
             'content-type': "application/json",
             'trn-api-key': api_key
@@ -19,15 +20,27 @@ class PUBG(Resource):
     def get(self):
 
         parser = reqparse.RequestParser()
-        parser.add_argument('player', required=True, type=str, location='args')
+        parser.add_argument('player', required=False, type=str, location='args')
+        parser.add_argument('steam', required=False, type=str, location='args')
 
         args = parser.parse_args(strict=False)
         player = args.get('player')
+        steam = args.get('steam')
 
-        try:
-            url = self.pubg_url + player
-            response = requests.request("GET", url, headers=self.headers)
-            return json.loads(response.text)
-        except BaseException as error:
-            print('Unhandled exception: ' + str(error))
-            raise
+        if player:
+            try:
+                url = self.pubg_url + player
+                response = requests.request("GET", url, headers=self.headers)
+                return json.loads(response.text)
+            except BaseException as error:
+                print('Unhandled exception: ' + str(error))
+                raise
+        if steam:
+            try:
+                url = self.steam_url + steam
+                response = requests.request("GET", url, headers=self.headers)
+                print response.text
+                return json.loads(response.text)
+            except BaseException as error:
+                print('Unhandled exception: ' + str(error))
+                raise
