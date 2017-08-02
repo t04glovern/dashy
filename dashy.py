795fe4a0ccbad07d579996f4e6ba1f35448b636d
Nathan Glover <nathan@glovers.id.au>
2017-08-03 00:02:54 +0800

    Changed api's

--- dashy.py
+++ dashy.py
@@ -94,7 +94,7 @@ def servers():
 @app.route("/ajax/nagios", methods=['GET'])
 @login_required
 def nagios():
-    url = "http://192.168.188.230:8080/state"
+    url = "http://nathanglover.com:8084/state"
     headers = {
         'content-type': "application/json"
     }
